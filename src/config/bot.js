59c03056e2df28bdd01d9cd7d9e149cab7e18875
Palmasterdraven <imitchell62791@gmail.com>
2026-07-15 11:45:22 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Looking at Pals",     // this is what people actually see
+        state: "Looking for Pals",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
